275f8c506eb02b762eca63aec8aadddf140fb783
JoeyM72 <joe72munley@gmail.com>
2022-03-17 13:13:14 -0400

    Basic setup

--- Code/movement/movement.ino
+++ Code/movement/movement.ino
@@ -1,27 +1,22 @@
 #include "Enes100.h"
 
+// Links
+// https://github.com/BlackrazorS2/ENES100-OSV
+// http://enes100.umd.edu/libraries/enes100
+
+    int TX_PIN = 2;
+    int RX_PIN = 4;
+    int SERVO_PIN = 6;
+
+    int MARKER_ID = 3;
+    double xPos, yPos, angle;
+   
 void setup() {
     // Initialize Enes100 Library
     // Team Name, Mission Type, Marker ID, TX Pin, RX Pin
-
-    int MARKER_ID = 3;
-    int TX_PIN = 2;
-    int RX_PIN = 4;
-    int location = new int[3];
-
     
-    Enes100.begin("DATA TEAM", DATA, MARKER_ID, TX_PIN, RX_PIN);
+    Enes100.begin("Frostbytes", DATA, MARKER_ID, TX_PIN, RX_PIN);
 
-    Enes100.print("Destination is at (");
-    Enes100.print(Enes100.destination.x);
-    Enes100.print(", ");
-    Enes100.print(Enes100.destination.y);
-    Enes100.println(")");
-    
-    // Transmit the message from the arduino
-    Enes100.mission('M');
-    // Any other setup code...
-    //comment
 }
 
 void loop() {
@@ -33,13 +28,13 @@ void loop() {
     Enes100.print("OSV is at (");
     Enes100.print(Enes100.location.x);
     Enes100.print(", ");
-    location[0] = Enes100.destination.x;
+    xPos = Enes100.location.x;
     Enes100.print(Enes100.location.y);
     Enes100.print(", ");
-    location[1] = Enes100.destination.y;
+    yPos = Enes100.location.y;
     Enes100.print(Enes100.location.theta);
     Enes100.println(")");
-    location[2] = Enes100.destination.theta;
+    angle = Enes100.location.theta;
 }
 
 void driveForwards() {
