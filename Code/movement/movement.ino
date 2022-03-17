73ff24d8ef31a1913849bf0ddbfef423c1e34c46
JoeyM72 <joe72munley@gmail.com>
2022-03-17 12:15:40 -0400

    Add Movement files

---
+++ Code/movement/movement.ino
@@ -0,0 +1,39 @@
+#include "Enes100.h"
+
+void setup() {
+    // Initialize Enes100 Library
+    // Team Name, Mission Type, Marker ID, TX Pin, RX Pin
+    Enes100.begin("DATA TEAM", DATA, 3, 10, 11);
+
+    Enes100.print("Destination is at (");
+    Enes100.print(Enes100.destination.x);
+    Enes100.print(", ");
+    Enes100.print(Enes100.destination.y);
+    Enes100.println(")");
+    
+    // Transmit the message from the arduino
+    Enes100.mission('M');
+    // Any other setup code...
+}
+
+void loop() {
+    // Update the OSV's current location
+    while (!Enes100.updateLocation()) {
+        // OSV's location was not found
+        Enes100.println("404 Not Found");
+    }
+    Enes100.print("OSV is at (");
+    Enes100.print(Enes100.location.x);
+    Enes100.print(", ");
+    Enes100.print(Enes100.location.y);
+    Enes100.print(", ");
+    Enes100.print(Enes100.location.theta);
+    Enes100.println(")");
+}
+
+void driveForwards() {
+  
+}
+void turn(int angle, boolean turningLeft) {
+  
+}
