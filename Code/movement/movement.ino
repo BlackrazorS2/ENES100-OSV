b625d34ef8a0dead1e3943ff2116c6de87fbf76c
JoeyM72 <joe72munley@gmail.com>
2022-03-31 13:42:54 -0400

    Magnet stuff

--- Code/movement/movement.ino
+++ Code/movement/movement.ino
@@ -3,14 +3,19 @@
 // Links
 // https://github.com/BlackrazorS2/ENES100-OSV
 // http://enes100.umd.edu/libraries/enes100
+// https://create.arduino.cc/projecthub/ryanchan/how-to-use-the-l298n-motor-driver-b124c5
 
-    int TX_PIN = 2;
-    int RX_PIN = 4;
+    int TX_PIN = 12;
+    int RX_PIN = 13;
     int SERVO_PIN = 6;
-    int LEFT_MOTOR = 11;
-    int RIGHT_MOTOR = 10;
+    int LEFT_MOTOR_PIN1 = 3, LEFT_MOTOR_PIN2 = 2;
+    int RIGHT_MOTOR_PIN1 = 5, RIGHT_MOTOR_PIN2 = 4;
 
-    int MARKER_ID = 10;
+    //analog pins
+    
+    int MAGNET_PIN = 1;
+
+    int MARKER_ID = 9;
     double xPos, yPos, angle;
     
     boolean startingPosition; //Top is true, bottom is false
@@ -24,27 +29,40 @@ void setup() {
     
     Enes100.begin("Frostbytes", DATA, MARKER_ID, TX_PIN, RX_PIN);
 
-    pinMode(LEFT_MOTOR, OUTPUT);
-    pinMode(RIGHT_MOTOR, OUTPUT);
-
+    pinMode(LEFT_MOTOR_PIN1, OUTPUT);
+    pinMode(LEFT_MOTOR_PIN2, OUTPUT);
+    pinMode(RIGHT_MOTOR_PIN1, OUTPUT);
+    pinMode(RIGHT_MOTOR_PIN2, OUTPUT);
+    pinMode(MAGNET_PIN, INPUT);
 }
 
 void loop() {
     // Update the OSV's current location
+
+    if (getMagnetism()) {
+      Enes100.println("Detected");
+    } else {
+      Enes100.println("Not");
+    }
+    delay(1000);
+    /*
+    
     if (resetLocation() == false) {
       // emergency reverse
       Enes100.println("BRUH HOW OUT OF AREANNA");
       Enes100.println("WRITE THIS LATER, PROLLY BACKUP OSV");
+      delay(1000);
+    } else {
+       Enes100.updateLocation();
+       Enes100.println("Location found");  //DELETE LATER 
+       Enes100.print("x Position is: ");
+       Enes100.println(xPos);
+       Enes100.print("y Position is: ");
+       Enes100.println(yPos);
+       Enes100.print("Current angle is: ");
+       Enes100.println(angle);
+       delay(1000);
     }
-      Enes100.updateLocation();
-      Enes100.println("Location found");  //DELETE LATER 
-      Enes100.print("x Position is: ");
-      Enes100.println(xPos);
-      Enes100.print("y Position is: ");
-      Enes100.println(yPos);
-      Enes100.print("Current angle is: ");
-      Enes100.println(angle);
-      
     //This portion of code determines starting side and gets us to the mission site.
     /*
     startingPosition = getStartingSide();
@@ -147,8 +165,11 @@ void driveForwards(double distance) {
   int initialXPos = xPos;
   int initialYPos = yPos;
   while (sqrt(sq(xPos - initialXPos) + sq(yPos - initialYPos)) < distance) {
-    analogWrite(LEFT_MOTOR,255);
-    analogWrite(RIGHT_MOTOR,255);
+    digitalWrite(LEFT_MOTOR_PIN1, HIGH);
+    digitalWrite(LEFT_MOTOR_PIN2, LOW);
+
+    digitalWrite(RIGHT_MOTOR_PIN1, HIGH);
+    digitalWrite(RIGHT_MOTOR_PIN2, LOW);
     resetLocation();
   }
 }
@@ -158,30 +179,39 @@ void driveReverse(double distance) {
   int initialXPos = xPos;
   int initialYPos = yPos;
   while (sqrt(sq(xPos - initialXPos) + sq(yPos - initialYPos)) < distance) {
-    analogWrite(LEFT_MOTOR,-255);
-    analogWrite(RIGHT_MOTOR,-255);
+    digitalWrite(LEFT_MOTOR_PIN1, LOW);
+    digitalWrite(LEFT_MOTOR_PIN2, HIGH);
+
+    digitalWrite(RIGHT_MOTOR_PIN1, LOW);
+    digitalWrite(RIGHT_MOTOR_PIN2, HIGH);
     resetLocation();
   }
 }
 
 //Left is true, right is false
-void turn(double newAngle, boolean turningLeft) {
+void turn(double turnAngle, boolean turningLeft) {
   resetLocation();
-  while (angle > newAngle * 1.05 || angle < newAngle * .95) { //Current margin of error is arbitrary, adjust later
+  while (angle > turnAngle * 1.05 || angle < turnAngle * .95) { //Current margin of error is arbitrary, adjust later
     if (turningLeft) {
       //Right wheels drive forward, left wheels drive backwards
-      analogWrite(LEFT_MOTOR,-255);
-      analogWrite(RIGHT_MOTOR,255);
+      digitalWrite(LEFT_MOTOR_PIN1, LOW);
+      digitalWrite(LEFT_MOTOR_PIN2, HIGH);
+
+      digitalWrite(RIGHT_MOTOR_PIN1, HIGH);
+      digitalWrite(RIGHT_MOTOR_PIN2, LOW);
     } else {
       //Left wheels drive forward, right wheels drive backwards
-      analogWrite(LEFT_MOTOR,255);
-      analogWrite(RIGHT_MOTOR,-255);
+      digitalWrite(LEFT_MOTOR_PIN1, HIGH);
+      digitalWrite(LEFT_MOTOR_PIN2, LOW);
+
+      digitalWrite(RIGHT_MOTOR_PIN1, LOW);
+      digitalWrite(RIGHT_MOTOR_PIN2, HIGH);
     }
     resetLocation();
   }
 }
 
-boolean dropArm(double angle) {
+boolean dropArm() {
   
 }
 
@@ -223,7 +253,13 @@ double getSignal() {
 }
 
 double getMagnetism() {
-  
+  if (digitalRead(MAGNET_PIN) == HIGH) {
+    Enes100.println("MAGNET DETECTED");
+    return true;
+  } else {
+    Enes100.println("NOT DETECTED");
+    return false;
+  }
 }
 
 // Might need a lot of thinking, probably just drive backwards tiny amount
